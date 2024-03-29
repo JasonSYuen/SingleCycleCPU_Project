67980cea4fbe70c58b63b0870851ff518f88cf0d
JasonSYuen <93394621+JasonSYuen@users.noreply.github.com>
2024-03-29 16:22:33 -0700

    Add files via upload

---
+++ Fetch.h
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+extern int pc;
+
+string Fetch()
+{
+
+    string text;
+    ifstream MyFile;
+
+    MyFile.open("sample_part1.txt");
+
+    int count = 0;
+    string line;
+    while (getline(MyFile, line))
+    {
+        // cout << line << endl;
+
+        if (count == pc)
+        {
+            text = line;
+        }
+        count += 4;
+    }
+
+    MyFile.close();
+    pc += 4;
+
+    int next_pc = pc + 4;
+
+    return text;
+}
