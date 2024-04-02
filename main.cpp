d5a8f86d500bcbb197bb6f2149dab79f647f695a
JasonSYuen <js99168@icloud.com>
2024-04-01 21:48:21 -0700

    main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
+#include "Fetch.h"
+#include "Execute.h"
+#include "Mem.h"
+#include "decode.h"
 
 using namespace std;
 
-int pc;
+int pc = 0;
 
 int main()
 {
     cout << "hello world" << endl;
+    string machine_code = Fetch();
+    cout << machine_code << endl;
+    string *test = decode(machine_code);
+    for (int i = 0; i < 6; i++)
+    {
+        cout << test[i] << endl;
+    }
+    // operation, rs1, rs2, rd, imm, alu_ctrl
 }
