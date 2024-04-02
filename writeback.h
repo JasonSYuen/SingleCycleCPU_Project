414f853d6cc07961d595e8e08e9661384206952e
GuiH823 <92610751+GuiH823@users.noreply.github.com>
2024-04-02 00:10:49 -0700

    Add files via upload

---
+++ writeback.h
@@ -0,0 +1,22 @@
+#ifndef WRITEBACK_H //guard
+#define WRITEBACK_H
+#include <string>
+#include "decode.h"
+
+using namespace std;
+
+extern int rf[32];
+extern int total_clock_cycles;
+
+void writeback(string comp, int mem, string dest, string type){
+    if(type == ""){
+        rf[bin_to_dec(dest)] = bin_to_dec(comp);
+    }
+    if(type == "lw"){
+        rf[bin_to_dec(dest)] = mem;
+    }
+
+    total_clock_cycles++;
+}
+
+#endif
