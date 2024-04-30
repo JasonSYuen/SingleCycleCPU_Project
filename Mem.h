2395f935786bb18ffdfaf006230d155ad7c019f3
JasonSYuen <js99168@icloud.com>
2024-04-30 13:50:23 -0700

    mem initialization

--- Mem.h
+++ Mem.h
@@ -13,33 +13,17 @@ extern string memWrite;
 extern string memToReg;
 extern string memRead;
 
-int Mem(string address, string lw_sw, string to_store)
+void initializeMem()
 {
-    // for testing purposes
-    // cout << "////" << endl;
     d_mem[28] = 5;
     d_mem[29] = 16;
-    int place_in_array = 0;
-
-    place_in_array = hex_to_dec(address) / 4;
-    // << place_in_array << endl;
-    // cout << " test " << endl;
-
-    if (lw_sw == "sw")
-    {
-        d_mem[place_in_array] = (bin_to_dec(to_store));
-        //cout << " in d-mem [" << place_in_array << "]" << d_mem[place_in_array] << endl;
-    }
-
-    return d_mem[place_in_array];
 }
 
 int Mem2(string address, int to_store)
 {
     // for testing purposes
     // cout << "////" << endl;
-    d_mem[28] = 5;
-    d_mem[29] = 16;
+
     int place_in_array = 0;
 
     place_in_array = hex_to_dec(address) / 4;
@@ -50,7 +34,7 @@ int Mem2(string address, int to_store)
     {
         d_mem[place_in_array] = (to_store);
         cout << "memory 0x" << address << " is modified to 0x" << dec_to_hex(to_store) << endl;
-        //cout << " in d-mem [" << place_in_array << "]" << d_mem[place_in_array] << endl;
+        // cout << " in d-mem [" << place_in_array << "]" << d_mem[place_in_array] << endl;
     }
 
     return d_mem[place_in_array];
